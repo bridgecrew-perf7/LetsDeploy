3e07c5d1e371ccf7e096188e032d5529fc9eb9f6
Fatih Gençaslan <handoreyn@Fatihs-MacBook-Pro.local>
2021-09-06 01:33:21 +0300

    shit.

--- Controllers/DeployController.cs
+++ Controllers/DeployController.cs
@@ -5,6 +5,6 @@ namespace LetsDeploy.Controllers
     [Route("[controller]")]
     public class DeployController : ControllerBase
     {
-        public IActionResult Get() => Ok("Deployed.");
+        public IActionResult Get() => Ok("Shit.");
     }
 }
